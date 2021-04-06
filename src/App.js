e80f0e2f916f03dab5492c480e6ab944d6cf943c
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-04-06 15:29:21 +0200

    Change h1 with useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [message, setMessage] = useState("Hello React");
   // Your code below
+
+  useEffect(() => {
+    setMessage("Hello Neue Fische!");
+  }, []);
 
   return (
     <div className="App">
