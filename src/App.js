c335485197a9a82d80f09c387cace80b43563ef8
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-04-06 17:46:19 +0200

    Adding url fetch - Total failure for LiftingStateUp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import UserList from "./components/UserList.js";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">Hello React</div>;
+  return;
+  <div className="App">
+    HELLO REACCCCT!!!
+    <UserList />
+  </div>;
 }
 
 export default App;
--- src/components/User.js
+++ src/components/User.js
@@ -1,3 +1,14 @@
-function User() {}
+import { useState } from "react";
+
+const [userId, setUserId] = useState();
+
+function User({ title, body }) {
+  return (
+    <section className="">
+      <h2>{title}</h2>
+      <p> {body} </p>
+    </section>
+  );
+}
 
 export default User;
