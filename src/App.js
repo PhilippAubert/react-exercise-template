84f581320e3548a412af57c2a3ccd9b51e5c2c3c
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-04-06 16:26:56 +0200

    Add eventlistener and useEffect for window.resize

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   // Your code below
 
+  useEffect(() => {
+    function handleWindowWidth() {
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleWindowWidth);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowWidth);
+      console.log("removed");
+    };
+  }, []);
+
+  // Effekt nur beim ersten Render! = > [] am Ende!
+  // Event-Listener bei resize! = setState zu Window.innerwidth!
+  // CLeanup_Function hinterher!!
+
+  // `window.addEventListener('event-name' handlerFunction)`
+
+  // `window.removeEventListener('event-name', handlerFunction)`
+
+  // `handlerFunction`
+
   return (
     <div className="App">
       <p>Window Width: {windowWidth} </p>
